42a8f47fffbc5a9daf8b5c08405bafe356d169d7
Герман <buhrus2@yandex.ru>
2020-04-13 23:37:50 +0400

    first commit

---
+++ src/sidebars/SidebarBuild.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Build',
+};
+</script>
+
+<style scoped>
+  .sidebar {
+    font-size: 50px;
+    transform: rotate(90deg);
+    color: green;
+  }
+</style>
